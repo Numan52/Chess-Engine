b65590561e1490221dba77fcf2301b2b1f12ae9e
Numan52 <numan.demir@gmx.at>
2024-11-27 22:57:02 +0100

    implemented simple evaluation function

--- src/main/java/org/example/PositionEvaluater.java
+++ src/main/java/org/example/PositionEvaluater.java
@@ -1,18 +1,29 @@
 package org.example;
 
+import org.example.Piece.Piece;
+import org.example.Piece.PieceType;
+
 public class PositionEvaluater {
-    private static final int PAWN_VALUE = 100;
-    private static final int ROOK_VALUE = 500;
-    private static final int KNIGHT_VALUE = 300;
-    private static final int BISHOP_VALUE = 300;
-    private static final int QUEEN_VALUE = 900;
     private Board board;
+    private int evaluation;
 
     public PositionEvaluater(Board board) {
         this.board = board;
     }
 
     public int evaluatePosition() {
+        for (Piece[] piecesRow : board.getBoardState()) {
+            for (Piece piece : piecesRow) {
+                if (piece != null && piece.getType() != PieceType.KING) {
+                    if (piece.getIsWhite()) {
+                        evaluation += piece.getValue();
+                    }
+                    else {
+                        evaluation -= piece.getValue();
+                    }
+                }
+            }
+        }
         return 0;
     }
 }
